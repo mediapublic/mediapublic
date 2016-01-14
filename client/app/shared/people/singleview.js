016d697929d997aaf9f5767cb37941cdb9fcadc5
Gabe Isman <gabe.isman@gmail.com>
2016-01-13 17:55:00 -0800

    Add admin controls for associated users.

--- client/app/shared/people/singleview.js
+++ client/app/shared/people/singleview.js
@@ -4,4 +4,29 @@ import template from './singletemplate.jade';
 export default ItemView.extend({
   template,
   className: 'person-container tile',
+  serializeData() {
+    let data = ItemView.prototype.serializeData.apply(this, arguments);
+    data.viewState.needsApproval = this.model.needsApproval() &&
+        (app.currentUser.isOrgAdmin(this.model.get('organization_id')) || app.currentUser.isAdmin());
+    return data;
+  },
+  events: {
+    'click .approve': function(event) {
+      this.model.set('org_approved', true);
+      this.model.save();
+      event.stopImmediatePropagation();
+      event.stopPropagation();
+      event.preventDefault();
+    },
+
+    'click .reject': function(event) {
+      this.model.set('organization_id', '');
+      this.model.save().then(() => {
+        this.model.collection.fetch();
+      });
+      event.stopImmediatePropagation();
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  },
 });
